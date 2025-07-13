5d5b2fd51b58da0e9b80550f51afb6ffd38d8e93
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-07-13 08:23:16 +0100

    Add page-layout component

---
+++ src/components/page-layout.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { useEffect, useState } from "react";
+import Footer from "./footer";
+import Navbar from "./navbar";
+import Loading from "./loading";
+
+export default function PageLayout({children,}: Readonly<{children: React.ReactNode;}>) {
+  const [showLoader, setShowLoader] = useState(true);
+    
+  useEffect(() => {
+    const handleLoad = () => {
+      setTimeout(() => setShowLoader(false), 500);
+    };
+
+    if (document.readyState === "complete") {
+      handleLoad();
+    } else {
+      window.addEventListener("load", handleLoad);
+      return () => window.removeEventListener("load", handleLoad);
+    }
+  }, []);
+
+  if (showLoader) return <Loading />
+  return(
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  )
+}
