607378dda655531c28c910684c7eca444c86d72e
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-07-13 08:08:10 +0100

    Add theme toggle component

---
+++ src/components/theme-toggle.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import * as React from "react"
+import {useTheme} from "next-themes"
+
+import {Button} from "@/components/ui/button"
+import {FaMoon, FaSun} from "react-icons/fa6";
+
+export default function ThemeToggle() {
+	const { theme, setTheme } = useTheme()
+
+	const toggleTheme = () => {
+		setTheme(theme === "light" ? "dark" : "light");
+	};
+
+	return (
+		<Button variant="outline"
+						size="icon"
+						onClick={toggleTheme}
+		>
+			{
+				theme === "light" ?
+					<FaMoon className="size-5" />
+					: <FaSun className="size-5" />
+			}
+			<span className="sr-only">Toggle theme</span>
+		</Button>
+	)
+}
