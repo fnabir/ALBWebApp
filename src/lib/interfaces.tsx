7fa6f39dbc57f410bbe7569eac8c0ac689af0603
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-08-03 22:21:23 +0100

    Create ProductInterface

--- src/lib/interfaces.tsx
+++ src/lib/interfaces.tsx
@@ -7,3 +7,10 @@ export interface BreadcrumbInterface {
   label: string;
   href?: string;
 }
+
+export interface ProductInterface {
+  label: string;
+  src?: string;
+  type?:string;
+  href?: string;
+}
