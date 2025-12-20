c8f54fcca850804eb2588f0ea2e2f9c750d5fdbe
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-12-20 11:37:37 +0000

    Revert "Fix Sicor image in partner section"
    This reverts commit 59eb8bd37ed25630041a96e32a8a54739602c790.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
@@ -10,6 +10,10 @@ const nextConfig: NextConfig = {
       {
         protocol: 'http',
         hostname: 'en.mdunks.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.sicoritaly.com',
       },
       {
         protocol: 'http',
@@ -32,7 +36,7 @@ const nextConfig: NextConfig = {
         hostname: 'cdn.globalso.com',
       },
     ],
-  },
+  }
 };
 
 export default nextConfig;
--- src/components/pages/home/partners-section.tsx
+++ src/components/pages/home/partners-section.tsx
@@ -1,60 +1,59 @@
-import { PageSection } from '@/components/page-section';
-import Image from 'next/image';
+import { PageSection } from "@/components/page-section";
+import Image from "next/image";
 
 const partners = [
   {
-    src: 'http://en.mdunks.com/templets/default/img/logo-s.png',
-    alt: 'Suzhou MODUN Express Elevator Co., Ltd.',
-    name: 'MODUN Elevator',
-    website: 'http://en.mdunks.com/',
+    src: "http://en.mdunks.com/templets/default/img/logo-s.png",
+    alt: "Suzhou MODUN Express Elevator Co., Ltd.",
+    name: "MODUN Elevator",
+    website: "http://en.mdunks.com/",
   },
   {
-    src: 'http://www.mdel.co.kr/simg/logo.png',
-    alt: 'MODEUN ELEVATOR CO.,LTD',
-    name: 'MODEUN Elevator',
-    website: 'http://www.mdel.co.kr/',
+    src: "http://www.mdel.co.kr/simg/logo.png",
+    alt: "MODEUN ELEVATOR CO.,LTD",
+    name: "MODEUN Elevator",
+    website: "http://www.mdel.co.kr/",
   },
   {
-    src: 'https://www.inovance.eu/typo3conf/ext/wr/Resources/Public/img/PAM_Logo.svg',
-    alt: 'Suzhou MONARCH Control Technology Co',
-    name: 'MONARCH Control',
-    website: 'https://www.inovance.eu/',
+    src: "https://www.inovance.eu/typo3conf/ext/wr/Resources/Public/img/PAM_Logo.svg",
+    alt: "Suzhou MONARCH Control Technology Co",
+    name: "MONARCH Control",
+    website: "https://www.inovance.eu/",
   },
   {
-    src: 'http://www.torindrive-europe.com/images/logo_TORIN_EUROPE.png',
-    alt: 'TORINDRIVE',
-    name: 'TORIN Drive',
-    website: 'http://www.torindrive-europe.com/',
+    src: "http://www.torindrive-europe.com/images/logo_TORIN_EUROPE.png",
+    alt: "TORINDRIVE",
+    name: "TORIN Drive",
+    website: "http://www.torindrive-europe.com/",
   },
   {
-    src: 'https://www.sicoritaly.com/wp-content/uploads/2021/08/logo-sicor-1-Modificata.png',
-    alt: 'SICOR ITALY',
-    name: 'SICOR Italy',
-    website: 'https://www.sicoritaly.com/',
-    unoptimized: true,
+    src: "https://www.sicoritaly.com/wp-content/uploads/2021/08/logo-sicor-1-Modificata.png",
+    alt: "SICOR ITALY",
+    name: "SICOR Italy",
+    website: "https://www.sicoritaly.com/",
   },
   {
-    src: 'http://www.shineelevator.com/img/upimages/home/20190525133629821.png',
-    alt: 'Beijing Shine Elevator Co., Ltd',
-    name: 'Shine Elevator',
-    website: 'http://www.shineelevator.com/',
+    src: "http://www.shineelevator.com/img/upimages/home/20190525133629821.png",
+    alt: "Beijing Shine Elevator Co., Ltd",
+    name: "Shine Elevator",
+    website: "http://www.shineelevator.com/",
   },
   {
-    src: 'https://shopsource.singoo.cc/801/general/Qezs8BNJe3ZxiKRf/xizi_0223.png?x-oss-process=image/resize,w_200/quality,q_100',
-    alt: 'Xizi Elevator Technology Co., Ltd',
-    name: 'Xizi Elevator',
-    website: 'https://www.xizielevator.com/',
+    src: "https://shopsource.singoo.cc/801/general/Qezs8BNJe3ZxiKRf/xizi_0223.png?x-oss-process=image/resize,w_200/quality,q_100",
+    alt: "Xizi Elevator Technology Co., Ltd",
+    name: "Xizi Elevator",
+    website: "https://www.xizielevator.com/",
   },
 ];
 
 export default function PartnerSection() {
   return (
-    <PageSection
+    <PageSection 
       id="partners"
       className="bg-muted/50"
       badge="Business Partner"
       heading="Our Valued Partners"
-      subheading="We're proud to collaborate with industry-leading companies and
+      subheading="We&apos;re proud to collaborate with industry-leading companies and
             organizations. These strong partnerships enable us to deliver
             superior elevator and lift solutions with unmatched quality and
             reliability."
@@ -75,15 +74,12 @@ export default function PartnerSection() {
                 fill
                 sizes="(max-width: 768px) 50vw, 20vw"
                 className="object-contain"
-                unoptimized={partner.unoptimized ?? false}
               />
             </div>
-            <div className="text-sm font-medium text-gray-700">
-              {partner.name}
-            </div>
+            <div className="text-sm font-medium text-gray-700">{partner.name}</div>
           </a>
         ))}
       </div>
     </PageSection>
-  );
+  )
 }
