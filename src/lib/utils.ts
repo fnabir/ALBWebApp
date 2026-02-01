8a43ffe9a591765cb2c9a3bc6c6f9f1d7166a7f0
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-02-01 15:23:50 +0000

    Change the database location to save forms

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,43 +1,47 @@
-import { database } from "@/firebase/config"
-import { clsx, type ClassValue } from "clsx"
-import { FirebaseError } from "firebase/app"
-import { child, push, ref, set } from "firebase/database"
-import { twMerge } from "tailwind-merge"
+import { database } from '@/firebase/config';
+import { clsx, type ClassValue } from 'clsx';
+import { FirebaseError } from 'firebase/app';
+import { child, push, ref, set } from 'firebase/database';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
-export async function addContactForm(data: object): Promise<"success"> {
+export async function addContactForm(data: object): Promise<'success'> {
   try {
-    const newRef = push(child(ref(database), "website/contact"))
+    const newRef = push(child(ref(database), 'forms/contact'));
     const dataWithDate = {
       ...data,
       date: new Date().toISOString(),
-    }
-    await set(newRef, dataWithDate)
-    return "success"
+    };
+    await set(newRef, dataWithDate);
+    return 'success';
   } catch (error: unknown) {
     if (error instanceof FirebaseError) {
-      throw new Error(error.message)
+      throw new Error(error.message);
     }
-    throw new Error("An unexpected error occurred while submitting the form. Please try again")
+    throw new Error(
+      'An unexpected error occurred while submitting the form. Please try again',
+    );
   }
 }
 
-export async function addQuoteForm(data: object): Promise<"success"> {
+export async function addQuoteForm(data: object): Promise<'success'> {
   try {
-    const newRef = push(child(ref(database), "website/quote"))
+    const newRef = push(child(ref(database), 'forms/quote'));
     const dataWithDate = {
       ...data,
       date: new Date().toISOString(),
-    }
-    await set(newRef, dataWithDate)
-    return "success"
+    };
+    await set(newRef, dataWithDate);
+    return 'success';
   } catch (error: unknown) {
     if (error instanceof FirebaseError) {
-      throw new Error(error.message)
+      throw new Error(error.message);
     }
-    throw new Error("An unexpected error occurred while submitting the form. Please try again")
+    throw new Error(
+      'An unexpected error occurred while submitting the form. Please try again',
+    );
   }
 }
